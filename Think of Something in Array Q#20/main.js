7769afb160b06b0bb7294e182b3eb0313f524603
SADIA BANO <sadiabano4257@gmail.com>
2024-07-06 04:01:48 +0500

    Add files via upload

---
+++ Think of Something in Array Q#20/main.js
@@ -0,0 +1,10 @@
+// Task # 20
+//Think of something you could store in a array. 
+//For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. 
+//Write a program that creates a list containing these items.
+let locations = ["Karachi", "Lahore", "Multan", "Islamabad", "Muree"];
+console.log("\nList of Locations are :\n");
+for (let i = 0; i < locations.length; i++) {
+    console.log(locations[i]);
+}
+export {};
