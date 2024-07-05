7300613adbd0c94728a5301f81e26f614e7e3967
SADIA BANO <sadiabano4257@gmail.com>
2024-07-06 03:40:46 +0500

    Add files via upload

---
+++ Dinner Guests Q#19/main.ts
@@ -0,0 +1,10 @@
+//Task #19
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18.
+//Taking Task # 14
+let Guestlist:string[]=["Ayesha","Saira","Yamna"];
+/*for(let i=0;i<Guestlist.length;i++){
+    console.log(`Dear ${Guestlist[i]}! \nYou are invited to Dinner.\n`);
+}*/
+//print a message indicating the number of people you are inviting to dinner.
+console.log(`The Total Numbers of People invited are :${Guestlist.length}` );
+
