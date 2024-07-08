0e7c0dd0a515522d0d7e9be933756607730a0061
SADIA BANO <sadiabano4257@gmail.com>
2024-07-09 01:49:58 +0500

    Add files via upload

---
+++ International Error Q #22/main.ts
@@ -0,0 +1,11 @@
+//Task # 22
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.
+//Change an index in one of your programs to produce an index error.
+let arr=["Sadia",12,"Student"]
+console.log("\nProduce an Index Eror\n")
+console.log(arr[3])
+//Make sure you correct the error before closing the program.
+console.log("\nCorrection in an Index Eror\n")
+console.log(arr[2])
+
+
