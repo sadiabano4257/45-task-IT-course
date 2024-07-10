17e2d1cb9c2b1536ac5d6eceaa2ac0a5a6d7a72f
SADIA BANO <sadiabano4257@gmail.com>
2024-07-11 00:43:53 +0500

    Add files via upload

---
+++ Alien Colors #1 Q #25/main.js
@@ -0,0 +1,17 @@
+//Task #25
+//Alien Colors #1: Imagine an alien was just shot down in a game. 
+//Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+let alian_color = "green";
+//• Write an if statement to test whether the alien’s color is green.
+if (alian_color === "green") 
+// If it is, print a message that the player just earned 5 points.
+{
+    console.log("congratulations! You just earned 5 points !");
+}
+//• Write one version of this program that passes the if test and another that fails. 
+//(The version that fails will have no output.)
+alian_color = "red";
+if (alian_color == "green") {
+    console.log("congratulations! You just earned 5 points !");
+}
+export {};
